cdb2b9ab8828a8a72a9db332744d7287760ace40
Anh <anhdlcwk@gmail.com>
2025-11-02 10:57:50 +0700

    cautructhumuc

---
+++ lib/router_theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+  textTheme: GoogleFonts.interTextTheme(),
+  useMaterial3: true,
+);
